bfffab5d4695d518e4cfab1bfc331cec63a0bb35
Joey Kaan <jkaan@users.noreply.github.com>
2018-02-27 21:43:43 +0100

    Encrypt password before sending into database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3 }
+  before_save :encrypt_password
+
+  def encrypt_password
+    if password.present?
+      self.password = BCrypt::Password.create(password)
+    end
+  end
 end
